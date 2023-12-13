3f420afbb3e64aa27bc0d5d657defb51ac8978b9
Harshitha M <harshithamuniraj22@gmail.com>
2023-12-13 23:10:53 +0530

    first commit

--- src/components/childComponent/Child.js
+++ src/components/childComponent/Child.js
@@ -1,12 +1,44 @@
 import React from 'react'
 import './child.css'
-const Child = ({SetInputState}) => {
+const Child = ({itemList,SetItemList}) => {
+ const removeItem=(pp)=>{
+const update=[]
+itemList.map((ii,index)=>{
+  console.log(pp,index)
+   if (index!=pp){
+      return update.push(ii)
+   }
+ })
+ SetItemList(update)
+ }
+  
   return (
     <>
-      <div className='children'>
+    <div className='children'>
         <h2>Children Component</h2>
-        <input type="text" placeholder='Enter' className='inp' onChange={(e)=>SetInputState(e.target.value)} />
-      </div>
+        
+     <div>
+      {
+        itemList.map((item,index)=>{
+          const {itemName,itemPrice}=item
+          return(
+            <>
+            <div  key={index} className='flex'>
+            <ul>
+              <li >
+                {itemName}- &#8377;{itemPrice}
+              </li>
+            </ul>
+            <button className='btn' onClick={()=>removeItem(index)}>Remove</button>
+            </div>
+            
+            </>
+          )
+        })
+      }
+       
+     </div>
+     </div>
     </>
   )
 }
--- src/components/parentComponent/Parent.js
+++ src/components/parentComponent/Parent.js
@@ -1,17 +1,57 @@
 import React, { useState } from 'react'
 import Child from '../childComponent/Child';
-import './parent.css'
+import './parent.css';
+
+// let items=JSON.parse(localStorage.getItem(items))
+// const list=(items)=>{
+//   console.log(items)
+//   if(items){
+//     return items
+//   }else{
+//     return []
+//   }
+
 const Parent = () => {
-    const [inputState,SetInputState]=useState("")
+    const [inputState,SetInputState]=useState('');
+    const [itemList,SetItemList]=useState([])
+   
+
+    
+    function updateInp(e){
+     
+      SetInputState({...inputState,[e.target.name]:e.target.value});
+      
+      // localStorage.setItem("input","inputState");
+      // kk
+    }
+    
+    function displayItem(e){
+      e.preventDefault();
+      console.log(inputState)
+      SetItemList([...itemList,inputState])
+      // localStorage.setItem('items', JSON.stringify(itemList));
+      SetInputState({itemName:'',itemPrice:''})
+    }
   return (
     <>
       <div className='parent'>
         <div className='parent-container'>
             <h1>Parent Component</h1>
-            <p>{inputState}</p>
-            <Child SetInputState={SetInputState}/>
+            <div>
+              <form onSubmit={displayItem}>
+                <label htmlFor="itemNname">Item Name</label>
+                <input type="text" name="itemName" value={inputState.itemName} required onChange={updateInp} id="itemNname" />
+                <label htmlFor="itemPrice">Item Price</label>
+                <input type="number" name="itemPrice" value={inputState.itemPrice} required onChange={updateInp} id="itemPrice" />
+                <button type='submit'>Add Item</button>
+              </form>
+            </div>
+            
+          
         </div>
+        <Child itemList={itemList} SetItemList={SetItemList}/>
       </div>
+     
     </>
   )
 }
