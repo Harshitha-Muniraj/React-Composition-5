7e3c0fd53adb40f42fe58a9503cbf0bcf025df63
Harshitha M <harshithamuniraj22@gmail.com>
2023-12-19 20:14:37 +0530

    lift state 5

--- src/components/childComponent/Child.js
+++ src/components/childComponent/Child.js
@@ -1,44 +1,25 @@
 import React from 'react'
 import './child.css'
-const Child = ({itemList,SetItemList}) => {
- const removeItem=(pp)=>{
-const update=[]
-itemList.map((ii,index)=>{
-  console.log(pp,index)
-   if (index!=pp){
-      return update.push(ii)
-   }
- })
- SetItemList(update)
- }
-  
+const Child = ({SetLogin}) => {
+
+  const loginCheck=(e)=>{
+    e.preventDefault();
+    SetLogin(true)
+  }
+
   return (
     <>
-    <div className='children'>
-        <h2>Children Component</h2>
-        
-     <div>
-      {
-        itemList.map((item,index)=>{
-          const {itemName,itemPrice}=item
-          return(
-            <>
-            <div  key={index} className='flex'>
-            <ul>
-              <li >
-                {itemName}- &#8377;{itemPrice}
-              </li>
-            </ul>
-            <button className='btn' onClick={()=>removeItem(index)}>Remove</button>
-            </div>
-            
-            </>
-          )
-        })
-      }
-       
-     </div>
-     </div>
+    <form onSubmit={loginCheck}>
+    <div>
+      <label>UserName:</label>
+      <input required/>
+    </div>
+    <div>
+      <label>PassWord</label>
+      <input required/>
+    </div>
+    <button type='submit'>Login</button>
+    </form>
     </>
   )
 }
--- src/components/parentComponent/Parent.js
+++ src/components/parentComponent/Parent.js
@@ -1,48 +1,13 @@
-import React, { useState } from 'react'
+import React, { useState } from 'react';
 import Child from '../childComponent/Child';
-import './parent.css';
-
-
 
 const Parent = () => {
-    const [inputState,SetInputState]=useState('');
-    const [itemList,SetItemList]=useState([])
-   
-
-    
-    function updateInp(e){
-     
-      SetInputState({...inputState,[e.target.name]:e.target.value});
-     
-    }
-    
-    function displayItem(e){
-      e.preventDefault();
-      console.log(inputState)
-      SetItemList([...itemList,inputState])
-    
-      SetInputState({itemName:'',itemPrice:''})
-    }
+  const [login,SetLogin]=useState(false)
   return (
     <>
-      <div className='parent'>
-        <div className='parent-container'>
-            <h1>Parent Component</h1>
-            <div>
-              <form onSubmit={displayItem}>
-                <label htmlFor="itemNname">Item Name</label>
-                <input type="text" name="itemName" value={inputState.itemName} required onChange={updateInp} id="itemNname" />
-                <label htmlFor="itemPrice">Item Price</label>
-                <input type="number" name="itemPrice" value={inputState.itemPrice} required onChange={updateInp} id="itemPrice" />
-                <button type='submit'>Add Item</button>
-              </form>
-            </div>
-            
-          
-        </div>
-        <Child itemList={itemList} SetItemList={SetItemList}/>
-      </div>
-     
+    <h1>Parent Component</h1>
+    {login?<h2>Login Successful</h2>:null}
+    {login?null:<Child SetLogin={SetLogin} />}
     </>
   )
 }
